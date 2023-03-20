c17ef927222a3b09392b30e201749144981ee711
wanghao <whateverhow@gmail.com>
2023-03-20 15:18:32 -0500

    with proxy

--- src/lib/axios/axiosRequest.ts
+++ src/lib/axios/axiosRequest.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { URL } from 'url';
 
 import {
   AxiosServerError,
@@ -7,6 +8,20 @@ import {
 
 export async function axiosRequest(options: AxiosRequestConfig): Promise<unknown> {
   try {
+    if (process.env.PROXY_SERVER_ADDR && !options.proxy) {
+      const proxyUrl = new URL(process.env.PROXY_SERVER_ADDR);
+      options.proxy = {
+        protocol: proxyUrl.protocol,
+        host: proxyUrl.host,
+        port: +proxyUrl.port,
+      }
+      if (proxyUrl.username) {
+        options.proxy.auth = {
+          username: proxyUrl.username,
+          password: proxyUrl.password,
+        }
+      }
+    }
     const response = await axios(options);
     return response.data;
   } catch (error) {
