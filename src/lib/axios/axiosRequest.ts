0974f6b1d4a80fd3481df02dd3ec6a4a4a7754fb
wanghao <whateverhow@gmail.com>
2023-08-24 18:11:33 -0500

    [with-proxy]   (use "git restore --staged <file>..." to unstage)

--- src/lib/axios/axiosRequest.ts
+++ src/lib/axios/axiosRequest.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
-const httpsProxyAgent = require('https-proxy-agent');
-
+import { HttpsProxyAgent } from 'https-proxy-agent';
 import {
   AxiosServerError,
   AxiosError,
@@ -9,7 +8,7 @@ import {
 export async function axiosRequest(options: AxiosRequestConfig): Promise<unknown> {
   try {
     if (process.env.PROXY_SERVER_ADDR) {
-      const httpsAgent = new httpsProxyAgent(process.env.PROXY_SERVER_ADDR);
+      const httpsAgent = new HttpsProxyAgent(process.env.PROXY_SERVER_ADDR);
       options.httpsAgent = httpsAgent
     }
     const response = await axios(options);
